195ae0e85fd43fd4225b6e8e34b738f18fa79c90
Connor Bentley Watson <cbentleywatson@gmail.com>
2022-03-30 12:43:30 -0400

    backe ned starting

---
+++ src/backend/server.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const app = express()
+const path = require('path');
+//app.set("view engine", "ejs")
+const port = process.env.PORT || 3000;
+
+/*
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
+*/
+/*
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+*/
+
+app.get("/", (req,res)=>{
+	console.log("Here")
+  // Correctly finds the html file...
+  //res.sendFile(path.join(__dirname, "../public", "index.html"));
+	
+  //res.send({message: "json"})
+//	res.se
+	//res.render("public/index")
+})
+
+/*
+
+app.get('/express_backend', (req, res) => { //Line 9
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
+});
+
+*/
+
+
+app.listen(3000)
+console.log("Hello")
