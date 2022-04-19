e7f9b7962dbe318114ca280b58ffc7383f0ede91
Lindsay Lord <lindsay.lord@ufl.edu>
2022-04-18 21:03:28 -0400

    special features;)

--- src/components/Show_Results.js
+++ src/components/Show_Results.js
@@ -5,36 +5,22 @@ import "./result_style.css";
 export default function Show_Results() {
     const DataC = [
         {
-          firstName: "Lindsay",
-          url: "https://i.pinimg.com/564x/b7/ef/91/b7ef914b4b9e35fdcd193612652dbe90.jpg",
-          lastName: "Lord",
-          year: "4th year",
-          dept: "Engineering", 
-          email: "lindsay.lord",
-          studyTimes: "Monday Tuesday Evening", 
-          locations: "Marston",
-          similarClass: "CEN3031",
-          virtual: "Zoom",
-          studySetting: "Group", 
-          problemApproach: "Theoretical",
-          outgoingness: "Extroverted", 
-          planning: "Plan Ahead"
-        },
-        {
             firstName: "Maya",
             lastName: "Harris",
             url: "https://i.pinimg.com/564x/02/3d/99/023d9921b290368909e47bb8b6e02f0d.jpg",
-            year: "3rd year",
+            year: "2nd year",
             dept: "Engineering", 
-            email: "blah",
-            studyTimes: "Monday Tuesday Evening", 
-            locations: "Marston",
+            email: "maya.harris@ufl.edu",
+            studyDays: "Tuesday, Thursday",
+            studyTimes: "Morning", 
+            locations: "Library West",
             similarClass: "CEN3031",
-            virtual: "Zoom",
-            studySetting: "Group", 
-            problemApproach: "Theoretical",
+            virtual: "In-person",
+            studySetting: "Individual", 
+            problemApproach: "Practical",
             outgoingness: "Extroverted", 
-            planning: "Plan Ahead"
+            planning: "Plan Ahead", 
+            score: "64"
           },
           {
             firstName: "Joe",
@@ -42,121 +28,154 @@ export default function Show_Results() {
             url: "https://i.pinimg.com/564x/f7/7e/b5/f77eb5b99a00ae191bca970db847a38d.jpg",
             year: "2nd year",
             dept: "Engineering", 
-            email: "cool_email",
-            studyTimes: "Monday Tuesday Evening", 
+            email: "jsulkes@ufl.edu",
+            studyTimes: "Monday, Friday",
+            studyDays: "Late Afternoon, Nightowl", 
             locations: "Marston",
             similarClass: "CEN3031",
             virtual: "Zoom",
             studySetting: "Group", 
             problemApproach: "Theoretical",
             outgoingness: "Extroverted", 
-            planning: "Plan Ahead"
+            planning: "Plan Ahead", 
+            score: "78"
+          },
+          {
+            firstName: "Lindsay",
+            url: "https://i.pinimg.com/564x/b7/ef/91/b7ef914b4b9e35fdcd193612652dbe90.jpg",
+            lastName: "Lord",
+            year: "4th year",
+            dept: "Engineering", 
+            email: "lindsay.lord@ufl.edu",
+            studyTimes: "Monday, Tuesday, Wednesday",
+            studyDays: "Evening", 
+            locations: "Classroom Hall",
+            similarClass: "CEN3031",
+            virtual: "In-Person",
+            studySetting: "Group", 
+            problemApproach: "Theoretical",
+            outgoingness: "Extroverted", 
+            planning: "Adapt as you go", 
+            score: "51"
+          }
+        
+    ];
+    const DataA = [
+          {
+            firstName: "Joe",
+            lastName: "Sulkes",
+            url: "https://i.pinimg.com/564x/f7/7e/b5/f77eb5b99a00ae191bca970db847a38d.jpg",
+            year: "2nd year",
+            dept: "Engineering", 
+            email: "jsulkes@ufl.edu",
+            studyTimes: "Monday, Friday",
+            studyDays: "Late Afternoon, Nightowl",
+            locations: "Marston",
+            similarClass: "CEN3031",
+            virtual: "Zoom",
+            studySetting: "Group", 
+            problemApproach: "Theoretical",
+            outgoingness: "Extroverted", 
+            planning: "Plan Ahead", 
+            score: "78"
+          },
+          {
+            firstName: "Lindsay",
+            url: "https://i.pinimg.com/564x/b7/ef/91/b7ef914b4b9e35fdcd193612652dbe90.jpg",
+            lastName: "Lord",
+            year: "4th year",
+            dept: "Engineering", 
+            email: "lindsay.lord@ufl.edu",
+            studyTimes: "Monday, Tuesday, Wednesday",
+            studyDays: "Evening", 
+            locations: "Classroom Hall",
+            similarClass: "CEN3031",
+            virtual: "In-Person",
+            studySetting: "Group", 
+            problemApproach: "Theoretical",
+            outgoingness: "Extroverted", 
+            planning: "Adapt as you go", 
+            score: "51"
+          },
+          {
+            firstName: "Maya",
+            lastName: "Harris",
+            url: "https://i.pinimg.com/564x/02/3d/99/023d9921b290368909e47bb8b6e02f0d.jpg",
+            year: "2nd year",
+            dept: "Engineering", 
+            email: "maya.harris@ufl.edu",
+            studyDays: "Tuesday, Thursday",
+            studyTimes: "Morning", 
+            locations: "Library West",
+            similarClass: "CEN3031",
+            virtual: "In-person",
+            studySetting: "Individual", 
+            problemApproach: "Practical",
+            outgoingness: "Extroverted", 
+            planning: "Plan Ahead", 
+            score: "64"
           },
         
     ];
-
-    const DataA = [
-        {
-            firstName: "Maya",
-            lastName: "Harris",
-            url: "https://i.pinimg.com/564x/02/3d/99/023d9921b290368909e47bb8b6e02f0d.jpg",
-            year: "3rd year",
-            dept: "Engineering", 
-            email: "blah",
-            studyTimes: "Monday Tuesday Evening", 
-            locations: "Marston",
-            similarClass: "CEN3031",
-            virtual: "Zoom",
-            studySetting: "Group", 
-            problemApproach: "Theoretical",
-            outgoingness: "Extroverted", 
-            planning: "Plan Ahead"
-          },
-        {
-          firstName: "Lindsay",
-          lastName: "Lord",
-          url: "https://i.pinimg.com/564x/b7/ef/91/b7ef914b4b9e35fdcd193612652dbe90.jpg",
-          year: "4th year",
-          dept: "Engineering", 
-          email: "lindsay.lord",
-          studyTimes: "Monday Tuesday Evening", 
-          locations: "Marston",
-          similarClass: "CEN3031",
-          virtual: "Zoom",
-          studySetting: "Group", 
-          problemApproach: "Theoretical",
-          outgoingness: "Extroverted", 
-          planning: "Plan Ahead"
-        },
+    const DataT = [
           {
             firstName: "Joe",
             lastName: "Sulkes",
             url: "https://i.pinimg.com/564x/f7/7e/b5/f77eb5b99a00ae191bca970db847a38d.jpg",
             year: "2nd year",
             dept: "Engineering", 
-            email: "cool_email",
-            studyTimes: "Monday Tuesday Evening", 
+            email: "jsulkes@ufl.edu",
+            studyTimes: "Monday, Friday",
+            studyDays: "Late Afternoon, Nightowl",
             locations: "Marston",
             similarClass: "CEN3031",
             virtual: "Zoom",
             studySetting: "Group", 
             problemApproach: "Theoretical",
             outgoingness: "Extroverted", 
-            planning: "Plan Ahead"
-          }
-        
-      ];
-      const DataT = [
-        {
-            firstName: "Joe",
-            lastName: "Sulkes",
-            url: "https://i.pinimg.com/564x/f7/7e/b5/f77eb5b99a00ae191bca970db847a38d.jpg",
-            year: "2nd year",
-            dept: "Engineering", 
-            email: "cool_email",
-            studyTimes: "Monday Tuesday Evening", 
-            locations: "Marston",
-            similarClass: "CEN3031",
-            virtual: "Zoom",
-            studySetting: "Group", 
-            problemApproach: "Theoretical",
-            outgoingness: "Extroverted", 
-            planning: "Plan Ahead"
+            planning: "Plan Ahead", 
+            score: "78"
           },
-        {
+          {
             firstName: "Maya",
             lastName: "Harris",
             url: "https://i.pinimg.com/564x/02/3d/99/023d9921b290368909e47bb8b6e02f0d.jpg",
-            year: "3rd year",
+            year: "2nd year",
             dept: "Engineering", 
-            email: "blah",
-            studyTimes: "Monday Tuesday Evening", 
-            locations: "Marston",
+            email: "maya.harris@ufl.edu",
+            studyDays: "Tuesday, Thursday",
+            studyTimes: "Morning", 
+            locations: "Library West",
             similarClass: "CEN3031",
-            virtual: "Zoom",
+            virtual: "In-person",
+            studySetting: "Individual", 
+            problemApproach: "Practical",
+            outgoingness: "Extroverted", 
+            planning: "Plan Ahead", 
+            score: "64"
+          },
+          {
+            firstName: "Lindsay",
+            url: "https://i.pinimg.com/564x/b7/ef/91/b7ef914b4b9e35fdcd193612652dbe90.jpg",
+            lastName: "Lord",
+            year: "4th year",
+            dept: "Engineering", 
+            email: "lindsay.lord@ufl.edu",
+            studyTimes: "Monday, Tuesday, Wednesday",
+            studyDays: "Evening", 
+            locations: "Classroom Hall",
+            similarClass: "CEN3031",
+            virtual: "In-Person",
             studySetting: "Group", 
             problemApproach: "Theoretical",
             outgoingness: "Extroverted", 
-            planning: "Plan Ahead"
-          },
-        {
-          firstName: "Lindsay",
-          lastName: "Lord",
-          url: "https://i.pinimg.com/564x/b7/ef/91/b7ef914b4b9e35fdcd193612652dbe90.jpg",
-          year: "4th year",
-          dept: "Engineering", 
-          email: "lindsay.lord",
-          studyTimes: "Monday Tuesday Evening", 
-          locations: "Marston",
-          similarClass: "CEN3031",
-          virtual: "Zoom",
-          studySetting: "Group", 
-          problemApproach: "Theoretical",
-          outgoingness: "Extroverted", 
-          planning: "Plan Ahead"
-        }
+            planning: "Adapt as you go", 
+            score: "51"
+          }
         
-      ];
+    ];
+    
+    
   // List of all cars satisfing all the filters
   const [filteredList, setFilteredList] = useState(DataT);
   // Selected filter
@@ -233,12 +252,14 @@ export default function Show_Results() {
               <div>
                   <img className="car-image" src= {item.url} alt= "car-img"/>
                   <h4 className="user-name">{`${item.firstName}${" " + item.lastName}`}</h4>
+                  <div className="user-score">{`${item.score}${"% compatible"}`}</div>
               </div>
             <div className="user-year">{`Year: ${item.year}`}</div>
             <div className="user-dept">{`Department: ${item.dept}`}</div>
-            <div className="user-virtual">{`Preference for: ${item.virtual}`}</div>
-            <div className="user-studySetting">{`Prefer to work in: ${item.studySetting}`}</div>
-            <div className="user-studySetting">{`Email address: ${item.email}`}</div>
+            <div className="user-studySetting">{`Prefer to work: ${item.studySetting}${" via " + item.virtual}`}</div>
+            <div className="user-studySetting">{`Days: ${item.studyDays}${"// Time:" + item.studyTimes}`}</div>
+            <div className="user-studySetting">{`Personality: ${item.problemApproach + ", " + item.outgoingness + ", " + item.planning}`}</div>
+            <div className="user-email">{`Email address: ${item.email}`}</div>
           </div>
         ))}
       </div>
