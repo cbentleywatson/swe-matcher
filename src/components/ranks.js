98aca346d68e1c0be5a9912c25938765b3541597
cbentleywatson <cbentleywatson@gmail.com>
2022-04-20 06:18:48 -0400

    ranks going in

--- src/components/ranks.js
+++ src/components/ranks.js
@@ -1,23 +1,36 @@
 const axios = require('axios');
 debug = true;
-function(cat, rank) {
-    route = "http://localhost:5000"
+async function getUser(cat, rank) {
+    route = "http://localhost:5000/cmp"
     target = route + "/" + cat + "/" + rank;
-    axios.get(target)
+    //back
+    console.log(target);
+    var data = "";
+    const d = await axios.get(target)
         .then((response) => {
             //alert("in response");
-            return response.data;
-            //alert(response.data);
+            let a = '';
+            //a = a + response.data;
+            //console.log(response.data);
+            data = response.data;
+
             //    alert(response);
             //alert("axios response");
-
+            return data;
             //this.setState({firstName: response.firstName, lastName: response.lastName, email: response.email})
         })
-        .catch((error) => {
-            alert("Error! " + error);
-            //alert("request " + request);
-            console.log(error);
-
-            return "";
-        })
+    console.log(data);
 }
+
+async function get_compatible_users_by_category(cat, number) {
+    var ar = [];
+    var cur;
+    for (let i = 0; i < number; i++) {
+        cur = await getUser("com1", 1);
+        ar.push(cur);
+        //console.log(cur);
+
+    }
+    //console.log("aa" + ar);
+}
+get_compatible_users_by_category("cat", 3);
