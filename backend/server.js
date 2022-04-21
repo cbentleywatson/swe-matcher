abb2923a14dbb77449c75110fbd61cb0a5d86d33
jsulkes20 <jsulkes20@gmail.com>
2022-04-20 20:49:17 -0400

    y

--- backend/server.js
+++ backend/server.js
@@ -527,19 +527,6 @@ firebase.initializeApp({
 var db = firebase.firestore();
 */
 
-
-app.route('/getUsers')
-  /* Basic Tests*/
-  .post(function (req, res) {
-    console.log("post found");
-    const three_array = {
-      "new_users_c": newUsersC,
-      "new_users_a": newUsersA,
-      "new_users_g": newUsersGeneralCompatibility
-    }
-    res.send(three_array);
-  });
-
 app.route('/simplest2')
   /* Basic Tests*/
   .get(function (req, res) {
@@ -621,56 +608,25 @@ router.get('/submit-user-results:results', function (req, res) {
   // console.log("last name: "+ req.params.last);
 });
 
+router.get('/getUser', function (req, res) {
 
+  const three_array = {
+    "new_users_c": newUsersC,
+    "new_users_a": newUsersA,
+    "new_users_g": newUsersGeneralCompatibility
+  };
 
-/*
-const bodyParser = require("body-parser");
-router.post(‘/login’,(req, res) => {
-    var user_name = req.body.user;
-    var password = req.body.password;
-    console.log(“User name = “+user_name+”, password is “+password);
-    res.end(“yes”);
-    });
-*/
-
-
-
-
-
-
-/* Basic Tests*/
-/*
-app.route('/survey-results-post')
-.post(function(req,res){
-  console.log("survey results input");
-  var survey_results = req.body;
-  var cur_results =[survey_results];
-  cur_results.forEach(function(obj) {
-    db.collection("users").add({
-        FirstName : obj.FirstName,
-        LastName: obj.LastName,
-        Year: obj.Year,
-        Department: obj.Department,
-        Email: obj.Email
-    }).then(function(docRef) {
-        console.log("Added a user");
-    })
-    .catch(function(error) {
-        console.error("Error adding user: ", error);
-    });
-  res.send("Posted!")
-});
-*/
-
-/* Backend proper */
-router.get('/match/id', function (req, res) {
-  res.send('hello ' + req.params.name + '!');
+  res.send(three_array);
+  
 });
 
 
-router.get('/response', function (req, res) {
-  res.send('hello ' + req.params.name + '!');
-});
+
+
+
+
+
+
 
 
 
@@ -678,76 +634,7 @@ router.get('/response', function (req, res) {
 app.use('/', router);
 
 
-/*
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
-    if (response.status !== 200) {
-      throw Error(body.message) 
-    }
-    return body;
-  };
-*/
-/*x
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-*/
 
-
-//app.get("/", (req,res)=>{
-//	console.log("in root")
-// Correctly finds the html file...
-//res.sendFile(path.join(__dirname, "../public", "index.html"));
-
-//res.send({message: "in root"})
-//	res.se
-//res.render("public/index")
-//});
-/*
-app.get("submit-survey-results", (request, res)=>{
-  console.log("survey-result-found");
-});
-*/
-/*
-app.get("/submit-survey-results", (req,res)=> {
-  
-  request (
-    "http://localhost:5000",
-    function(error,response,body){
-      res.send({message:"5000"})
-      if(!error &&response.statusCode == 200){
-        var parsedBody =JSON.parse(body);
-        console.log("in survery results");
-        //var temp_c = parsedBody["current"]["temp_c"]
-        //res.send({temp_c})
-        
-      }
-    })
-   
-});
-*/
-/*
-app.get("/", (req,res)=>{
-  console.log("in second get")
-  // Correctly finds the html file...
-  //res.sendFile(path.join(__dirname, "../public", "index.html"));
-	
-  res.send({message: "in second get"})
-//	res.se
-  //res.render("public/index")
-});
-*/
-
-
-/*
-app.get('/express_backend', (req, res) => { //Line 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-});
-*/
-
-/* Starts the server listening on port 3000  */
 console.log("Hello");
 //
-app.listen(5000);
+app.listen(5001);
